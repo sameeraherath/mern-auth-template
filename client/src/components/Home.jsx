97b270ff29f8323dd05010fdb178e3cbed82d8b6
sameeraherath <sameerastar6@gmail.com>
2025-02-13 10:27:15 +0530

    Update Home.jsx

---
+++ client/src/components/Home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <div className="max-w-md mx-auto p-4">
+      <h1 className="text-3xl text-center font-semibold">Home Page</h1>
+      <p className="text-center mt-4">Welcome to the home page!</p>
+    </div>
+  );
+};
+
+export default Home;
